6c84e838a5088850375a1d639a4f26a6e3a39a9b
Son Vu <vungocson94@gmail.com>
2014-04-15 23:57:59 +0700

    change position of alert box

--- protected/views/welcomepage/fb.php
+++ protected/views/welcomepage/fb.php
@@ -1,7 +1,7 @@
-<div class="g-form-row">
+<div class="g-form-row" style="padding-bottom: 35px">
     <form action="<?php echo Yii::app()->createUrl('welcomepage/fb_login') ?>">
         <div class="g-form-row-field">
-            <button class="g-btn type_primary size_small" style="background-color: #1265A8" type="submit">
+            <button class="g-btn type_primary size_small" style="background-color: #1265A8; width: 100%" type="submit">
                 <i class="icon-facebook"></i>
                 <span>Đăng Nhập qua Facebook</span>
             </button>
