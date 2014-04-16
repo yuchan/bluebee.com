4b13fbe6248114668e56f988f05a00765a0f9b7b
huy <huynt57@gmail.com>
2014-04-16 09:03:59 +0700

    fix create class controller

--- protected/controllers/ClassPageController.php
+++ protected/controllers/ClassPageController.php
@@ -1,6 +1,8 @@
 <?php
 
-class ClassPageController extends Controller {
+Yii::import('application.controllers.BaseController');
+
+class ClassPageController extends BaseController {
 
     public function actionIndex() {
         $this->actionClassPage();
@@ -21,14 +23,37 @@ class ClassPageController extends Controller {
                     'description' => @$_POST['description'],
                 );
                 if (!empty($loginFormData['classcode'])) {
-                    $newclass = class_model::model()->findByAttributes(array('class_code' => $createClassFormData['classcode']));
+                    $newclass = ClassYear::model()->findByAttributes(array('class_code' => $createClassFormData['classcode']));
                     if ($newclass) {
-                        $this->retVal->message = "Mã lớp đã tồn tại";
+                        if ($newclass->class_year == date("Y")) {
+                            $this->retVal->message = "Mã lớp cho năm học này đã tồn tại, bạn hãy tìm xem lớp mình ở đâu nhé";
+                            $this->retVal->success = 0;
+                        } else {
+                            $this->retVal->message = "Mã lớp cho năm học này chưa tồn tại, nhưng đã có từ các năm học trước. Bạn có muốn bluebee lấy tài liệu của các năm trước về ?";
+                        }
                     } else {
-                        
+                        $class_new = new class_model;
+                        $class_year = new ClassYear;
+
+                        $class_new->class_code = $createClassFormData['classcode'];
+                        $class_new->class_name = $createClassFormData['classname'];
+                        $class_new->class_description = $createClassFormData['description'];
+
+                        $class_new->save(FALSE);
+                        $class_id = class_model::model()->findByAttributes(array('class_id' => $class_new->class_id));
+
+                        $class_year->class_code = $createClassFormData['classcode'];
+                        $class_year->class_name = $createClassFormData['classname'];
+                        $class_year->class_code = $class_id->class_id;
+
+                        $class_year->save(FALSE);
+
+                        $this->retVal->message = "Tạo lớp thành công, chúc bạn học tập tốt với bluebee";
+                        $this->retVal->success = 1;
                     }
                 } else {
                     $this->retVal->message = "Mã lớp không được để trống";
+                    $this->retVal->success = 0;
                 }
             } catch (exception $e) {
                 $this->retVal->message = $e->getMessage();
