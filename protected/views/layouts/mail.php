54f18b6953f663fcffd5d823062ad9a0f137a10b
Nguyen Duy Kien <kienduynguyen94@gmail.com>
2014-04-17 22:34:22 +0700

    no message

---
+++ protected/views/layouts/mail.php
@@ -0,0 +1,49 @@
+<!-- CANVAS -->
+<div class="l-canvas type_wide col_cont headerpos_fixed headertype_extended">
+    <div class="l-canvas-h">
+
+        <!-- HEADER -->
+        <div class="l-header">
+            <div class="l-header-h">
+                <img src="<?php echo Yii::app()->theme->baseUrl; ?>/assets/img/demo/mail.jpg"/>
+            </div>
+        </div>
+
+
+
+
+        <!-- FOOTER -->
+        <div class="l-footer type_normal">
+            <div class="l-footer-h">
+
+                <!-- subfooter: top -->
+                <div class="l-subfooter at_top">
+                    <div class="l-subfooter-h g-cols cols_fluid">
+
+                        <div class="l-footer-h">
+                            This notification has been sent to the email address associated with your website's account. 
+                            For information on privacy policy, visit http://blue-bee.uet.com. 
+                            This email message was auto-generated. Please do not respond.
+
+                        </div>
+                    </div>
+                </div>
+                <div class="l-subfooter at_bottom">
+                    <div class="l-subfooter-h i-cf">
+                        <div class='one-fourth'>
+                            <img style="float: left" src='<?php echo Yii::app()->theme->baseUrl; ?>/assets/img/demo/logo_1.jpg'/>
+                        </div>
+
+                        <div class='three-fourths'>
+                            <div class="w-copyright"> © 2014 All rights reserved. <br/> <a href='http://us-themes.com/'>BlueBee Team - K57CA - UET</a></div>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+
+    </div>
+</div>
