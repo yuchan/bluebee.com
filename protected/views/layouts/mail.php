e49edd2930c49439b14bb7c3b88743044a701689
Sonvn <vungocson94@gmail.com>
2014-04-17 22:27:29 +0700

    add mail template to invite friends

---
+++ protected/views/layouts/mail.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="en" />
+
+        <!-- blueprint CSS framework -->
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <link rel="stylesheet" type="text/css" media="all" href="http://fonts.googleapis.com/css?family=Open+Sans:400,700,400italic,700italic" />
+        <link rel="stylesheet" type="text/css" media="all" href="http://fonts.googleapis.com/css?family=Roboto+Slab:400,300,700" />
+    </head>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
+<body>
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+	}
+    div {
+	display: block;
+    }
+    .fix-margin-div {
+	margin: 20px;
+	clear: both;
+    }
+    .body {
+	border: 2px solid #429edb;
+	margin: 50px auto auto auto;
+	border-radius: 5px;
+	max-width: 600px;
+	font-family: proxima_nova,'Open Sans','Lucida Grande','Segoe UI',Arial,Verdana,'Lucida Sans Unicode',Tahoma,'Sans Serif';
+    }
+    p {
+    	font-size: 14px;
+    	color: white;
+    }
+    /*.header {
+	border-bottom: 2px dashed #429edb;
+    }*/
+    .clearfix:after {
+	clear: both;
+	content: ".";
+	display: block;
+	height: 0;
+	line-height: 0;
+	visibility: hidden;
+    }
+    .logo {
+	float: left
+
+    }
+    .sub-footer {
+	float: right;
+    }
+    .main {
+	clear: both;
+	background-color: #429edb;
+	min-height: 130px;
+    }
+    .fix-margin-logo {
+	margin: auto;
+	float: left;
+    }
+    a {
+    	text-decoration: none;
+    }
+    button {
+    	background-color: white;
+    	color: #429edb;
+    	border: none;
+    	outline: none;
+    	height: 50px;
+    	width: 80px;
+    	margin-left: 10px;
+    	font-size: 16px;
+    	border-radius: 5px;
+    }
+</style>
+
+<div class="body">
+    <div class="header">
+	<div class="fix-margin-div clearfix">
+	    <div class="logo" style="display: block; height: 34px;">
+		<a href="bluebee-uet.com" style="position: relative">
+		    <div style="float: left; margin: auto">
+				<img id="logo" height="40px" width="40px" src="http://bluebee-uet.com/themes/classic/assets/img/logo.jpg"/>
+		    </div>
+		    <div style="float: left; margin-top: 10px; position: relative">
+				<p style="color: #429edb">BlueBee-uet.com</p>
+		    </div>
+		</a>
+	    </div>
+	    <div class="sub-footer" style="margin: auto">
+		<a href=""><img title="Facebook " src="https://ci5.googleusercontent.com/proxy/AdXYYWi6KfFe4TIuRnSQWGpehzttcMpkfw1FFZPf1IK4uuduOqAIWEGNVnbWeECVIbpOqTmpzklxOGUxHjADlc--suadk0BSST6Cx-Ic6phiiyDFDRuddd8pHHAiY5enmL0T05aqQqAKM1UbjKnG2yKQ3TYnLaDNzIYSICuRM-KMEd44kmdJTnZEv2KqxAvJDA=s0-d-e1-ft#http://static.sendgrid.com/uploads/UID_1131154_NL_2570008_ff73fbeeda7cd25cc908e6b63bb046f8/ed8a3813eca4c554f170a70a6fadef35" border="0" alt="Facebook Logo" width="30" height="30"></a>
+		<a href=""><img title="Twitter" src="https://ci4.googleusercontent.com/proxy/k0HCOPoCqJpSVdZYTuGiaqgZ1h309hfAkyfvib_64y2HBTTJSaPtphtOLdXnlqDXn_0joYP-F_3qfS4XKu9hWQZor-RSUZO_Q2k0QippDiWfMfmEEgN7yyf_qqwDNTYw9eRaXK0N-SD4EdJasTl6LdT73TEsFIjVZtddSEt2aAYjYehwDT1-pUouiHPAZFtcJA=s0-d-e1-ft#http://static.sendgrid.com/uploads/UID_1131154_NL_2570008_ff73fbeeda7cd25cc908e6b63bb046f8/f28629cad2c7bd36f95fa131d6563516" border="0" alt="Twitter Logo" width="30" height="30"></a>
+		<a href=""><img title="linkedin" src="https://ci4.googleusercontent.com/proxy/TaS4IcG_KeKhvkSOvAaPjArDzpRBtevIao_cJWlsuor8nn7JkOWDMSGF_4hxj6XCMRMKTnpGKIhanJLv_lSoBiVCfc5qIp7BT9AV47t0I_67JtUAZJoOd4GdpdGjt8Gh5jYIlqWM_NahTC7EW2tEGWJMlssGWzuOaubxh-yyTa8XHl7p8Mar55AbdA9Y4xDieQ=s0-d-e1-ft#http://static.sendgrid.com/uploads/UID_1131154_NL_2570008_ff73fbeeda7cd25cc908e6b63bb046f8/7efc81b7c6c149ec0669feb28feb6eaa" border="0" alt="LinkedIn Logo" width="30" height="30"></a>
+		<a href=""><img title="GooglePlus" src="https://ci5.googleusercontent.com/proxy/6-eMPZp6ZbL3ucaQXVH06orfghRlMB5TKqKKSfejbCGixzq_hjuWNvhNtyuKqpbNoBXYT8rbQNOSDZuQfNm82jgOdMd4wAlY83aB8OsMWA8uWD1JW_le1yGyDy5cWNeVCYAdL4JzCSkaiaaCD0XLVYuXVCb0FY6NZpTlnJVEMsU71EWQAQsWleRYZ8Y69aU0MQ=s0-d-e1-ft#http://static.sendgrid.com/uploads/UID_1131154_NL_2570008_ff73fbeeda7cd25cc908e6b63bb046f8/cd2af9cab2c56adc973cf210736823bc" border="0" alt="GooglePlus Logo" width="30" height="30"></a>
+	    </div>
+	</div>
+    </div>
+    <div class="main">
+    	<style>
+    		.User a img {
+    			border-radius: 50%;
+    			height: 70px;
+    			width: 70px;
+    			margin-top: -10px;
+    		}
+    	</style>
+        <div class="fix-margin-div" style="padding-top: 20px">
+        	<div class="clearfix">
+        		<p style="color: white; margin-bottom: 20px; font-size: 14px">Hi huynt57@gmail.com,</p>
+               	<div class="User" style="float: left">
+                            <a href="user.bluebee-uet.com">
+                                <img src="https://fbcdn-profile-a.akamaihd.net/hprofile-ak-ash1/t1.0-1/c0.0.160.160/p160x160/1964973_758228940861481_2115701723_n.jpg"/>
+                            </a>
+                        </div>
+                        <div class="content" style="float: left; margin: 5px 0 0 10px; color: white">
+                        	<a href="user.bluebee-uet.com"><p style="color: white; text-decoration: underline; font-size: 20px;">User</p></a>
+                            <p style="font-size:13px;">User want to add You to Him/Her group/class</p>
+                        </div>
+                        <div class="button" style="margin: auto; float: right">
+        		            <button>Accept</button>
+        		            <button>Deny</button>
+        		        </div>
+        		<p style="margin-top: 100px; clear: both;">Thanks!</p>
+        		<p style="margin-bottom: 20px">- The BlueBee Team</p>
+            </div>
+        </div>
+
+    </div>
+    <div>
+
+    </div>
+</div>
+</body>
+</html>
