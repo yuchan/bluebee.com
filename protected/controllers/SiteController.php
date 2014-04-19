ebd8aee54041aafd010d0b66d2d9764d93b7cf82
khanhnv3007 <khanhnv3007@gmail.com>
2014-04-20 00:23:51 +0700

    Fix language in SiteController

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -58,7 +58,7 @@ class SiteController extends Controller {
                         "Content-Type: text/plain; charset=UTF-8";
 
                 mail(Yii::app()->params['adminEmail'], $subject, $model->body, $headers);
-                Yii::app()->user->setFlash('contact', 'Thank you for contacting us. We will respond to you as soon as possible.');
+                Yii::app()->user->setFlash('contact', 'Cảm ơn những góp ý của bạn. Chúng tôi sẽ phản hồi sớm nhất.');
                 $this->refresh();
             }
         }
