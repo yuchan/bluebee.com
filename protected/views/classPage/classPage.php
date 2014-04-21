20548e45dfafec201a33e1241cc71685a243063d
Son Vu <vungocson94@gmail.com>
2014-04-21 23:13:58 +0700

    add text area to form change information

--- protected/views/classPage/classPage.php
+++ protected/views/classPage/classPage.php
@@ -49,7 +49,8 @@
                         }
                     }});
             });
-        });</script>
+        });
+    </script>
 
 
 
@@ -97,6 +98,18 @@
                         </div>
                     </div>
                 </div>
+                <div class="g-form-group">
+                                    <div class="g-form-row-label">
+                                        <label class="g-form-row-label-h" for="description">Miêu tả (*)</label>
+                                    </div>
+                                    <div class="g-form-group-rows">
+                                        <div class="g-form-row">
+                                            <div class="g-form-row-field">
+                                                <textarea name="description" id="input1x3" cols="30" rows="10"></textarea>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                 <div class="g-form-row">
                     <div class="g-form-row-field">
                         <button class="g-btn type_primary" type="submit" name="Submit" value="Submit" style="text-transform: inherit" action="">Lưu thông tin</button>
