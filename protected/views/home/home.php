31dddfaa05b129989e423b6c78631e7fcd93952d
Nguyen Duy Kien <kienduynguyen94@gmail.com>
2014-04-22 21:31:10 +0700

    fix layout of create class form

--- protected/views/home/home.php
+++ protected/views/home/home.php
@@ -25,55 +25,11 @@
                                         </div>
                                         <div class="w-iconbox-text  color_alternate">
                                             <div class="w-iconbox-text-description">
-                                                <a class="popup-with-form" href="#test-modal">
+                                                <a class="popup-with-form" href="#newclassform">
                                                     <button class="g-btn type_primary size_big"><span><i class="icon-heart"></i>Tạo nhóm học tập</span></button>
                                                 </a>
 
-                                                <form class="g-form white-popup-block mfp-hide" id="novaLinha">
-                                                    <h3>Tạo nhóm mới</h3>
-                                                    <div class="g-form-group">
-                                                        <div class="g-form-group-rows">
-                                                            <div class="g-form-row">
-                                                                <div class="g-form-row-label">
-                                                                    <label class="g-form-row-label-h" for="class_name">Mã lớp</label>
-                                                                </div>
-                                                                <div class="g-form-row-field">
-                                                                    <div class="g-input">
-                                                                        <input type="text" name="classcode" id="contact_username" placeholder="Name" value="">
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-                                                            <div class="g-form-row">
-                                                                <div class="g-form-row-label">
-                                                                    <label class="g-form-row-label-h" for="contact_email">Tên lớp</label>
-                                                                </div>
-                                                                <div class="g-form-row-field">
-                                                                    <div class="g-input">
-                                                                        <input type="text" name="classname" id="contact_username" placeholder="Email" value="">
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-                                                            <div class="g-form-group">
-                                                                <div class="g-form-row-label">
-                                                                    <label class="g-form-row-label-h" for="contact_email">Miêu tả</label>
-                                                                </div>
-                                                                <div class="g-form-group-rows">
-                                                                    <div class="g-form-row">
-                                                                        <div class="g-form-row-field">
-                                                                            <textarea name="description" id="input1x3" cols="30" rows="10"></textarea>
-                                                                        </div>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-                                                            <div class="g-form-row">
-                                                                <div class="g-form-row-field">
-                                                                    <button class="g-btn type_primary" type="submit" name="Submit" value="Submit">Xác nhận</button>
-                                                                </div>
-                                                            </div>
-
-                                                        </div>
-                                                    </div>
-                                                </form>
+                                                <?php $this->renderPartial('partial/newclass')?>
                                             </div>
                                         </div>
                                     </div>
