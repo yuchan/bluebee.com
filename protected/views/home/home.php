be963cca64a2c1e1564942f6e829727dd8a1fbf1
Nguyen Duy Kien <kienduynguyen94@gmail.com>
2014-04-22 21:25:24 +0700

    change form to create class!

--- protected/views/home/home.php
+++ protected/views/home/home.php
@@ -29,50 +29,50 @@
                                                     <button class="g-btn type_primary size_big"><span><i class="icon-heart"></i>Tạo nhóm học tập</span></button>
                                                 </a>
 
-                                                <form class="g-form white-popup-block mfp-hide" id="test-modal">
-                                                    <h3>Create A New Class</h3>
+                                                <form class="g-form white-popup-block mfp-hide" id="novaLinha">
+                                                    <h3>Tạo nhóm mới</h3>
                                                     <div class="g-form-group">
                                                         <div class="g-form-group-rows">
                                                             <div class="g-form-row">
                                                                 <div class="g-form-row-label">
-                                                                    <label class="g-form-row-label-h" for="class_name">Class' Name</label>
+                                                                    <label class="g-form-row-label-h" for="class_name">Mã lớp</label>
                                                                 </div>
                                                                 <div class="g-form-row-field">
                                                                     <div class="g-input">
-                                                                        <input type="text" name="classname" id="contact_username" placeholder="Name" value="">
+                                                                        <input type="text" name="classcode" id="contact_username" placeholder="Name" value="">
                                                                     </div>
                                                                 </div>
                                                             </div>
                                                             <div class="g-form-row">
                                                                 <div class="g-form-row-label">
-                                                                    <label class="g-form-row-label-h" for="contact_email">Email</label>
+                                                                    <label class="g-form-row-label-h" for="contact_email">Tên lớp</label>
                                                                 </div>
                                                                 <div class="g-form-row-field">
                                                                     <div class="g-input">
-                                                                        <input type="text" name="email" id="contact_username" placeholder="Email" value="">
+                                                                        <input type="text" name="classname" id="contact_username" placeholder="Email" value="">
                                                                     </div>
                                                                 </div>
                                                             </div>
                                                             <div class="g-form-group">
                                                                 <div class="g-form-row-label">
-                                                                    <label class="g-form-row-label-h" for="contact_email">Describle</label>
+                                                                    <label class="g-form-row-label-h" for="contact_email">Miêu tả</label>
                                                                 </div>
                                                                 <div class="g-form-group-rows">
                                                                     <div class="g-form-row">
                                                                         <div class="g-form-row-field">
-                                                                            <textarea name="#" id="input1x3" cols="30" rows="10"></textarea>
+                                                                            <textarea name="description" id="input1x3" cols="30" rows="10"></textarea>
                                                                         </div>
                                                                     </div>
                                                                 </div>
                                                             </div>
                                                             <div class="g-form-row">
                                                                 <div class="g-form-row-field">
-                                                                    <button class="g-btn type_primary" type="submit" name="Submit" value="Submit">Submit</button>
+                                                                    <button class="g-btn type_primary" type="submit" name="Submit" value="Submit">Xác nhận</button>
                                                                 </div>
                                                             </div>
+
                                                         </div>
                                                     </div>
-
                                                 </form>
                                             </div>
                                         </div>
