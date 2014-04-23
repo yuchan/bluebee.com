90038fa385176ff82011288f1a70b434de86f570
khanhnv3007 <khanhnv3007@gmail.com>
2014-04-23 08:40:03 +0700

    fix style of avatar

--- themes/classic/views/layouts/main.php
+++ themes/classic/views/layouts/main.php
@@ -268,7 +268,7 @@
 
                                             <div class="w-nav-item level_1">
                                                 <div class="w-nav-item-h">
-                                                    <a href="<?php echo Yii::app()->createUrl("user?token=" . Yii::app()->session['token']) ?>" class="w-nav-anchor level_1">
+                                                    <a href="<?php echo Yii::app()->createUrl("user?token=" . Yii::app()->session['token']) ?>" class="w-nav-anchor level_ava">
                                                         <img style="border: 5px solid white;"class="ava" src="<?php
                                                         if (Yii::app()->session['user_avatar'] == "") {
                                                             echo Yii::app()->theme->baseUrl, "/assets/img/logo.jpg";
