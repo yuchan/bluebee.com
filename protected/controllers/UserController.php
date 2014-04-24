100076ca35d65195d6a17a58c10763f7f43ad1e2
huy <huynt57@gmail.com>
2014-04-24 16:28:27 +0700

    continue fix redirect if not login

--- protected/controllers/UserController.php
+++ protected/controllers/UserController.php
@@ -1,10 +1,7 @@
 <?php
 
 class UserController extends Controller {
-public function beforeAction() {
-        if (Yii::app()->user->isGuest)
-            $this->redirect('welcomePage');
-    }
+
     public function actionIndex() {
         $this->actionUser();
     }
