66823cfcb4e79d715b67e399544f54e02fc3f2d5
Son Vu <vungocson94@gmail.com>
2014-04-25 07:22:50 +0700

    commit

---
+++ protected/views/welcomePage/verifyaccount.php
@@ -0,0 +1,16 @@
+
+<body>
+    <p style="front-size: 13px">
+        Chào kiengio, 
+        
+        Để giúp bảo vệ thông tin trong tài khoản của bạn ở website, hãy xác minh địa chỉ email đăng kí của bạn bằng cách click vào link dưới đây. 
+        
+        <?php //echo $link?>
+        
+        Địa chỉ email của bạn sẽ được sử dụng để trợ giúp bạn trong quá trình thay đổi thông tin hoặc lấy lại quyền truy cập vào tài khoản.
+        Cảm ơn khi đã giúp chúng tôi bảo mật tài khoản của bạn.
+        
+        Trợ giúp người dùng
+        http://www.bluebee-uet.com 
+    </p>
+</body>
