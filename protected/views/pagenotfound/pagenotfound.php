3e0f61bbaa5d7d30dd009e493604926778c53c92
Sonvn <vungocson94@gmail.com>
2014-04-28 15:22:26 +0700

    add view pagenotfound

---
+++ protected/views/pagenotfound/pagenotfound.php
@@ -0,0 +1,13 @@
+<div class="l-main-h">
+	<div class="l-submain">
+		<div class="l-submain-h g-html">
+		
+			<div class="page-404">
+				<i class="icon-compass"></i>
+				<h1>Error 404 - page not found</h1>
+				<p />Ohh... You have requested the page that is no longer there.<p />
+			</div>
+		
+		</div>
+	</div>
+</div>
