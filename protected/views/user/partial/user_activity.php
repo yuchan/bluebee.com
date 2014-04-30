e9dcf15bd05ef9cd1a2319b268c5c938970961af
huynt <huynt57@gmail.com>
2014-04-30 16:42:24 +0700

    update user activity in user page

--- protected/views/user/partial/user_activity.php
+++ protected/views/user/partial/user_activity.php
@@ -1,8 +1,58 @@
-<?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<div class="rounded1 color_alternate">
+    <h6>Hoạt động gần đây</h6>
+</div>
+<?php foreach ($user_activity as $user_activity_info) : ?>
+    <div class="w-blog imgpos_attop meta_all">
+        <div class="w-blog-h">
+            <div class="w-blog-list">
+                <div class="w-blog-entry">
+                    <div class="w-blog-entry-h">
 
+                        <div class="w-blog-entry-body">
+                            <div class="w-blog-entry-meta">
+                                <div class="w-blog-entry-meta-date">
+                                    <i class="icon-time"></i>
+                                    <span class="w-blog-entry-meta-date-month"><?php echo $user_activity_info->post_date ?></span>
+
+                                </div>
+
+                                <div class="w-blog-entry-meta-comments">
+                                    <i class="icon-comments"></i>
+                                    <a class="w-blog-entry-meta-comments-h" href="javascript:void(0);">13 comments</a>
+                                </div>
+                            </div>
+
+                            <div class="w-blog-entry-short">
+                                <p><?php echo $user_activity_info->post_content ?></p>
+                            </div>
+
+                            <a href="blog-post.html">
+                                <button class="g-btn type_primary size_small"><span><i class="icon-star-empty"></i>Read more</span></button> 
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <?php endforeach; ?>
+        <div class="w-blog-pagination">
+            <div class="g-pagination">
+                <a href="javascript:void(0);" class="g-pagination-item to_prev">Prev</a>
+                <a href="javascript:void(0);" class="g-pagination-item">1</a>
+                <a href="javascript:void(0);" class="g-pagination-item active">2</a>
+                <a href="javascript:void(0);" class="g-pagination-item">3</a>
+                <a href="javascript:void(0);" class="g-pagination-item">4</a>
+                <a href="javascript:void(0);" class="g-pagination-item">5</a>
+                <a href="javascript:void(0);" class="g-pagination-item to_next">Next</a>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
--- protected/views/user/user.php
+++ protected/views/user/user.php
@@ -179,120 +179,12 @@
                                                                 </div>
                                 
                                                                 <a href="" style="margin-left: 30px">More...</a>-->
-
-                                <div class="rounded1 color_alternate">
-                                    <h6>Hoạt động gần đây</h6>
-                                </div>
-
-                                <div class="w-blog imgpos_attop meta_all">
-                                    <div class="w-blog-h">
-                                        <div class="w-blog-list">
-                                            <div class="w-blog-entry">
-                                                <div class="w-blog-entry-h">
-                                                    <a class="w-blog-entry-link" href="blog-post.html">
-                                                        <h2 class="w-blog-entry-title">
-                                                            <span class="w-blog-entry-title-h">This is a Single Clean Post</span>
-                                                        </h2>
-                                                    </a>
-                                                    <div class="w-blog-entry-body">
-                                                        <div class="w-blog-entry-meta">
-                                                            <div class="w-blog-entry-meta-date">
-                                                                <i class="icon-time"></i>
-                                                                <span class="w-blog-entry-meta-date-month">March</span>
-                                                                <span class="w-blog-entry-meta-date-day">23,</span>
-                                                                <span class="w-blog-entry-meta-date-year">2013</span>
-                                                            </div>
-
-                                                            <div class="w-blog-entry-meta-author">
-                                                                <i class="icon-user"></i>
-                                                                <a class="w-blog-entry-meta-author-h" href="javascript:void(0);">Admin</a>
-                                                            </div>
-
-                                                            <div class="w-blog-entry-meta-tags">
-                                                                <i class="icon-tags"></i>
-                                                                <a href="javascript:void(0);">Web Design</a>,
-                                                                <a href="javascript:void(0);">Branding</a>
-                                                            </div>
-
-                                                            <div class="w-blog-entry-meta-comments">
-                                                                <i class="icon-comments"></i>
-                                                                <a class="w-blog-entry-meta-comments-h" href="javascript:void(0);">13 comments</a>
-                                                            </div>
-                                                        </div>
-
-                                                        <div class="w-blog-entry-short">
-                                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis, sem quis lacinia faucibus, orci ipsum gravida tortor, vel interdum mi sapien ut justo. Nulla varius consequat magna, id molestie ipsum volutpat quis. Suspendisse consectetur fringilla suctus. Pellentesque ipsum erat, facilisis ut venenatis eu, sodales vel dolor.</p>
-                                                        </div>
-
-                                                        <a href="blog-post.html">
-                                                            <button class="g-btn type_primary size_small"><span><i class="icon-star-empty"></i>Read more</span></button> 
-                                                        </a>
-                                                    </div>
-                                                </div>
-                                            </div>
-
-                                            <div class="w-blog-entry">
-                                                <div class="w-blog-entry-h">
-                                                    <a class="w-blog-entry-link" href="blog-post.html">
-                                                        <h2 class="w-blog-entry-title">
-                                                            <span class="w-blog-entry-title-h">Another Single Interesting Post</span>
-                                                        </h2>
-                                                    </a>
-                                                    <div class="w-blog-entry-body">
-                                                        <div class="w-blog-entry-meta">
-                                                            <div class="w-blog-entry-meta-date">
-                                                                <i class="icon-time"></i>
-                                                                <span class="w-blog-entry-meta-date-month">March</span>
-                                                                <span class="w-blog-entry-meta-date-day">23,</span>
-                                                                <span class="w-blog-entry-meta-date-year">2013</span>
-                                                            </div>
-
-                                                            <div class="w-blog-entry-meta-author">
-                                                                <i class="icon-user"></i>
-                                                                <a class="w-blog-entry-meta-author-h" href="javascript:void(0);">Admin</a>
-                                                            </div>
-
-                                                            <div class="w-blog-entry-meta-tags">
-                                                                <i class="icon-tags"></i>
-                                                                <a href="javascript:void(0);">Web Design</a>,
-                                                                <a href="javascript:void(0);">Branding</a>
-                                                            </div>
-
-                                                            <div class="w-blog-entry-meta-comments">
-                                                                <i class="icon-comments"></i>
-                                                                <a class="w-blog-entry-meta-comments-h" href="javascript:void(0);">13 comments</a>
-                                                            </div>
-                                                        </div>
-
-                                                        <div class="w-blog-entry-short">
-                                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis, sem quis lacinia faucibus, orci ipsum gravida tortor, vel interdum mi sapien ut justo. Nulla varius consequat magna, id molestie ipsum volutpat quis. Suspendisse consectetur fringilla suctus. Pellentesque ipsum erat, facilisis ut venenatis eu, sodales vel dolor.</p>
-                                                        </div>
-
-                                                        <a href="blog-post.html">
-                                                            <button class="g-btn type_primary size_small"><span><i class="icon-star-empty"></i>Read more</span></button> 
-                                                        </a>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="w-blog-pagination">
-                                            <div class="g-pagination">
-                                                <a href="javascript:void(0);" class="g-pagination-item to_prev">Prev</a>
-                                                <a href="javascript:void(0);" class="g-pagination-item">1</a>
-                                                <a href="javascript:void(0);" class="g-pagination-item active">2</a>
-                                                <a href="javascript:void(0);" class="g-pagination-item">3</a>
-                                                <a href="javascript:void(0);" class="g-pagination-item">4</a>
-                                                <a href="javascript:void(0);" class="g-pagination-item">5</a>
-                                                <a href="javascript:void(0);" class="g-pagination-item to_next">Next</a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-
+                                <?php $this->renderPartial('partial/user_activity', array('user_activity' => $user_activity)) ?>
                             </div>
                         </div>
                     </div>
                 </div>
+
 
                 <div class="one-third">
 
