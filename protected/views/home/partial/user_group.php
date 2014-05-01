3408cfdd6409f9076f40859e81ad61fa3cb0b485
Khanh NGUYEN <khanhnv3007@gmail.com>
2014-05-01 21:14:00 +0700

    delete some redundant code in user_group

--- protected/views/home/partial/user_group.php
+++ protected/views/home/partial/user_group.php
@@ -20,35 +20,7 @@
                                                 </a>
                                             </div>
                                         </div>
-
-                                        <div class="w-portfolio-item naming webdesign">
-                                            <div class="w-portfolio-item-h animate_afc">
-                                                <a class="w-portfolio-item-anchor" href="project-another-slider.html">
-                                                    <div class="w-portfolio-item-image">
-                                                        <img src="<?php echo Yii::app()->theme->baseUrl; ?>/assets/img/demo/portfolio-1.jpg" alt="" />
-                                                        <div class="w-portfolio-item-meta">
-                                                            <h2 class="w-portfolio-item-title">Vietnam National University</h2>
-                                                            <i class="icon-mail-forward"></i>
-                                                        </div>
-                                                    </div>
-                                                </a>
-                                            </div>
-                                        </div>
-
-                                        <div class="w-portfolio-item naming branding">
-                                            <div class="w-portfolio-item-h animate_afc d1">
-                                                <a class="w-portfolio-item-anchor" href="project-slider.html">
-                                                    <div class="w-portfolio-item-image">
-                                                        <img src="<?php echo Yii::app()->theme->baseUrl; ?>/assets/img/demo/portfolio-2.jpg" alt="" />
-                                                        <div class="w-portfolio-item-meta">
-                                                            <h2 class="w-portfolio-item-title">Kien's Group</h2>
-                                                            <i class="icon-mail-forward"></i>
-                                                        </div>
-                                                    </div>
-                                                </a>
-                                            </div>
-                                        </div>
-
+                                        
                                     </div>
                                 </div>
                             </div>
