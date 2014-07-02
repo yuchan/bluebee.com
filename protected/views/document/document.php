e37457dec74bc7912839c5f2a51959a1ccc9cc58
Son Vu <vungocson94@gmail.com>
2014-07-02 23:45:29 +0700

    done upload form

--- protected/views/document/document.php
+++ protected/views/document/document.php
@@ -309,9 +309,43 @@
                 <div class="morph-content">
                     <div class="content-style-text">
                         <span class="icon icon-close">✕</span>
-                        <h4>Hãy chọn một tài liệu mà bạn muốn đăng</h4>
-                        <input type="button" class="g-btn size_small type_secondary" placeholder="Chọn 1 tệp tin"></input>
-
+                        <h4 style="margin-bottom: 5px;">Hãy chọn một tài liệu mà bạn muốn đăng</h4>
+                        <script type="text/javascript">
+                            $(document).ready(function() {
+                                $('.ssa').change(function(){
+                                    var file = this.files[0];
+                                    var file_size = file.size/1024;
+                                    file_size = (Math.round(file_size*100)/100);
+                                    $('.upload_button').hide();
+                                    $('.file_name').html(' '+file.name+' ');
+                                    $('.file_size').html(' - '+file_size+'KB');
+                                    $('.info_file').show();
+                                });
+                                $('.cancel_file').click(function(event) {
+                                    $('.info_file').hide();
+                                    $('.upload_button').show();
+                                });
+                            });
+                        </script>
+                        <label class="g-btn size_small type_primary upload_button">
+                            <input class="ssa" type="file" multiple="multiple" name="uploadedfile[]" style="display: none;" />
+                            <span>Chọn 1 tệp tin</span>
+                        </label>
+                        <div class="info_file" style="display: none">
+                            <span class="file_name"></span>
+                            <span class="file_size"></span>
+                            <span class="cancel_file">✕</span>
+                        </div>
+                        <input id="name_document" type="text" placeholder="Tên tài liệu"/>
+                        <textarea id="description_document" placeholder="Mô tả"></textarea>
+                        <select style="max-width: 200px; border-radius: 3px; background-color: white; margin: 5px 0;">
+                            <!--thêm môn học tại đây-->
+                            <option>Môn học</option>
+                            <option value="1">Công nghệ thông tin</option>
+                            <option value="2">Vật lý kĩ thuật</option>
+                        </select>
+                        <br/>
+                        <button class="g-btn size_small type_primary" type="submit">Đăng tài liệu</button>
                     </div>
                 </div>
             </div><!-- morph-button -->
