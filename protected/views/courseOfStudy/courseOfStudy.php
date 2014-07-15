1e26c423e9acaca05e6d6817f426ab7f0fa57dd0
Nguyen Duy Kien <kienduynguyen94@gmail.com>
2014-07-15 20:15:45 +0700

    fix page

--- protected/views/courseOfStudy/courseOfStudy.php
+++ protected/views/courseOfStudy/courseOfStudy.php
@@ -13,16 +13,61 @@
                         </p>
 
                         <h3 style="color: #262626 ;margin-top: 25px">Mô tả khái quát</h3>
-                        <p style="margin-right: 20px">
-                            <strong>1. Về kiến thức</strong> <br/>
-                            Sinh viên tốt nghiệp được trang bị kiến thức có hệ thống và hiện đại, phù hợp với các chương trình đào tạo tiên tiến trên thế giới.  <br/>
-                            •	Kiến thức tổng hợp về toán, khoa học tự nhiên, khoa học xã hội và nhân văn, ngoại ngữ. <br/>
-                            •	Kiến thức nền tảng trong KHMT như cơ sở toán trong KHMT, lập trình, cấu trúc dữ liệu và giải thuật, lý thuyết thông tin, chương trình dịch, trí tuệ nhân tạo. Đối với kiến thức sâu về ngành, tập trung đào chuyên sâu theo định hướng “Các hệ thống thông minh” và “Tương tác người máy” như xử lý ngôn ngữ tự nhiên, học máy, nhận dạng mẫu, tin sinh, xử lý tiếng nói, xử lý ảnh, tương tác người máy tính, lập trình trò chơi.<br/>
-                            •	Kiến thức tổng quan khác trong CNTT như công nghệ phần mềm, cơ sở dữ liệu, mạng máy tính, kiến trúc máy tính thông qua tỉ lệ môn học lựa chọn cao cùng với số lượng các môn học lựa chọn phong phú.<br/>
-                            •	Sinh viên được chú trọng đào tạo về kỹ năng lập trình với các ngôn ngữ, môi trường lập trình tiên tiến, tỉ trọng thực hành cao và nhiều bài tập ứng dụng thực tế. <br/>
-                            •	Sinh viên được đào tạo tăng cường tiếng Anh để có khả năng tự cập nhật kiến thức và làm việc trong môi trường CNTT trên thế giới. <br/>
-                            <br/>
-                        </p>
+                        <div class="w-tabs layout_accordion" style="margin-right: 20px;">
+                            <div class="w-tabs-h">
+                                <div class="w-tabs-section active">
+                                    <div class="w-tabs-section-title">
+                                        <span class="w-tabs-section-title-icon"></span>
+                                        <span class="w-tabs-section-title-text"><strong>1. Về kiến thức</strong></span>
+                                    </div>
+                                    <div class="w-tabs-section-content">
+                                        <div class="w-tabs-section-content-h">
+                                            <p>Sinh viên tốt nghiệp được trang bị kiến thức có hệ thống và hiện đại, phù hợp với các chương trình đào tạo tiên tiến trên thế giới.  <br/>
+                                                •	Kiến thức tổng hợp về toán, khoa học tự nhiên, khoa học xã hội và nhân văn, ngoại ngữ. <br/>
+                                                •	Kiến thức nền tảng trong KHMT như cơ sở toán trong KHMT, lập trình, cấu trúc dữ liệu và giải thuật, lý thuyết thông tin, chương trình dịch, trí tuệ nhân tạo. Đối với kiến thức sâu về ngành, tập trung đào chuyên sâu theo định hướng “Các hệ thống thông minh” và “Tương tác người máy” như xử lý ngôn ngữ tự nhiên, học máy, nhận dạng mẫu, tin sinh, xử lý tiếng nói, xử lý ảnh, tương tác người máy tính, lập trình trò chơi.<br/>
+                                                •	Kiến thức tổng quan khác trong CNTT như công nghệ phần mềm, cơ sở dữ liệu, mạng máy tính, kiến trúc máy tính thông qua tỉ lệ môn học lựa chọn cao cùng với số lượng các môn học lựa chọn phong phú.<br/>
+                                                •	Sinh viên được chú trọng đào tạo về kỹ năng lập trình với các ngôn ngữ, môi trường lập trình tiên tiến, tỉ trọng thực hành cao và nhiều bài tập ứng dụng thực tế. <br/>
+                                                •	Sinh viên được đào tạo tăng cường tiếng Anh để có khả năng tự cập nhật kiến thức và làm việc trong môi trường CNTT trên thế giới. <br/>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="w-tabs-section">
+                                    <div class="w-tabs-section-title">
+                                        <span class="w-tabs-section-title-icon"></span>
+                                        <span class="w-tabs-section-title-text"><strong>2. Về năng lực:  Sinh viên ra trường có</strong></span>
+                                        <span class="w-tabs-section-title-control"><i class="icon-angle-down"></i></span>
+                                    </div>
+                                    <div class="w-tabs-section-content">
+                                        <div class="w-tabs-section-content-h">
+                                            <p>
+                                                •	Tư duy logic tốt, có năng lực sáng tạo để giải quyết các bài toán ứng dụng cụ thể. có năng lực tự học để nắm bắt các công nghệ, công cụ, kỹ năng mới trong phát triển phần mềm. <br/>
+                                                •	Năng lực làm việc với vị trí lập trình viên trình độ cao cho các công ty phát triển phần mềm và hệ thống của các công ty trong và ngoài nước. Đặc biệt thích hợp cho các vị trí trong các lĩnh vực đòi hỏi công nghệ hiện đại và sáng tạo như phát triển các hệ thống thông minh, tương tác người máy. <br/>
+                                                •	Khả năng làm việc ở nhiều vị trí khác nhau trong các cơ quan tổ chức phát triển và ứng dụng CNTT hàng đầu trong nước.   <br/>
+                                                •	Sinh viên được trang bị kiến thức nền tảng và một số chuyên đề chuyên sâu trong KHMT, vì vậy có nhiều thuận lợi trong việc học lên thạc sĩ và tiến sĩ ngành KHMT và trở thành nhà nghiên cứu, giảng viên các trường đại học. <br/>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="w-tabs-section">
+                                    <div class="w-tabs-section-title">
+                                        <span class="w-tabs-section-title-icon"></span>
+                                        <span class="w-tabs-section-title-text"><strong>3. Về thái độ: Cử nhân ngành KHMT có</strong></span>
+                                        <span class="w-tabs-section-title-control"><i class="icon-angle-down"></i></span>
+                                    </div>
+                                    <div class="w-tabs-section-content">
+                                        <div class="w-tabs-section-content-h">
+                                            <p>
+                                                •	Phẩm chất chính trị tốt <br/>
+                                                •	Ý thức tổ chức kỷ luật, có tác phong làm việc khoa học, nghiêm túc, có đạo đức nghề nghiệp về bảo vệ thông tin, bản quyền.<br/>
+                                                •	Tinh thần làm việc theo nhóm, rèn luyện thường xuyên tính kỷ luật và khả năng giao tiếp.<br/>
+
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div class="one-third">
                         <div class="box more-box">
@@ -67,20 +112,9 @@
                 </div>
                 <div class="g-cols">
                     <div class="full-width">
-                        <strong>2. Về năng lực:  Sinh viên ra trường có</strong> <br/>
-                        •	Tư duy logic tốt, có năng lực sáng tạo để giải quyết các bài toán ứng dụng cụ thể. có năng lực tự học để nắm bắt các công nghệ, công cụ, kỹ năng mới trong phát triển phần mềm. <br/>
-                        •	Năng lực làm việc với vị trí lập trình viên trình độ cao cho các công ty phát triển phần mềm và hệ thống của các công ty trong và ngoài nước. Đặc biệt thích hợp cho các vị trí trong các lĩnh vực đòi hỏi công nghệ hiện đại và sáng tạo như phát triển các hệ thống thông minh, tương tác người máy. <br/>
-                        •	Khả năng làm việc ở nhiều vị trí khác nhau trong các cơ quan tổ chức phát triển và ứng dụng CNTT hàng đầu trong nước.   <br/>
-                        •	Sinh viên được trang bị kiến thức nền tảng và một số chuyên đề chuyên sâu trong KHMT, vì vậy có nhiều thuận lợi trong việc học lên thạc sĩ và tiến sĩ ngành KHMT và trở thành nhà nghiên cứu, giảng viên các trường đại học. <br/>
-                        <br/>
-                        <strong>3. Về thái độ: Cử nhân ngành KHMT có</strong> <br/>
-                        •	Phẩm chất chính trị tốt <br/>
-                        •	Ý thức tổ chức kỷ luật, có tác phong làm việc khoa học, nghiêm túc, có đạo đức nghề nghiệp về bảo vệ thông tin, bản quyền.<br/>
-                        •	Tinh thần làm việc theo nhóm, rèn luyện thường xuyên tính kỷ luật và khả năng giao tiếp.<br/>
-
                         <h3 style="color: #262626 ;margin-top: 25px">Chuẩn đầu ra của chương trình đào tạo</h3>
                         <p>
-                            <a href="#">Chuẩn đầu ra của chương trình đào tạo</a> giúp sinh viên có thể đảm nhận :<br/>
+                            <a href="#">Chuẩn đầu ra của chương trình đào tạo (download)</a>  giúp sinh viên có thể đảm nhận :<br/>
                             -	Giảng viên về nhóm ngành Máy tính và Công nghệ Thông tin<br/>
                             -	Chuyên gia nghiên cứu và phát triển về Công nghệ Thông tin <br/>
                             -	Trưởng nhóm phát triển phần mềm<br/>
@@ -94,7 +128,7 @@
                         <h3 style="color: #262626 ;margin-top: 25px">Liên hệ</h3>
                         <p>
                             Nếu bạn có thắc mắc về ngành Khoa học máy tính , hãy liên hệ :<br/>
-                            
+
                             <strong>Đại học Công Nghệ - Đại học Quốc Gia Hà Nội</strong><br/>
                             Khoa học máy tính <br/>
                             Số 2, Xuân Thủy, Cầu Giấy, Hà Nội<br/>
