30ad7d4040710bb5206530da5ef8d3041b805c37
kiennd57 <kienduynguyen94@gmail.com>
2014-07-29 22:01:57 +0700

    department page!

--- protected/views/share/teacherListPage.php
+++ protected/views/share/teacherListPage.php
@@ -8,49 +8,10 @@
     <div class="l-submain">
         <div class="l-submain-h g-html i-cf">
             <div class="g-cols">
-                <div class="one-fourth">
-                    <div class="teacherList-nav">
-                        <h4 class="header-color"> Danh sách các khoa:</h4>
-                        <div class="navigation roundedBorder">
-
-
-                            <div class="navigationLinks" id="additionalGenres">
-                                <div class="navRow">
-                                    <a href="/user/rate_books?genre=poetry" class="actionLinkLite " id="nav_to_poetry" onclick="rateBooksToGenre('poetry', '/user/rate_books?genre=poetry');
-                                            return false;">Poetry</a>
-                                </div>
-                                <div class="navRow">
-                                    <a href="/user/rate_books?genre=psychology" class="actionLinkLite " id="nav_to_psychology" onclick="rateBooksToGenre('psychology', '/user/rate_books?genre=psychology');
-                                            return false;">Psychology</a>
-                                </div>
-                                <div class="navRow">
-                                    <a href="/user/rate_books?genre=religion" class="actionLinkLite " id="nav_to_religion" onclick="rateBooksToGenre('religion', '/user/rate_books?genre=religion');
-                                            return false;">Religion</a>
-                                </div>
-                                <div class="navRow">
-                                    <a href="/user/rate_books?genre=sports" class="actionLinkLite " id="nav_to_sports" onclick="rateBooksToGenre('sports', '/user/rate_books?genre=sports');
-                                            return false;">Sports</a>
-                                </div>
-                                <div class="navRow">
-                                    <a href="/user/rate_books?genre=thriller" class="actionLinkLite " id="nav_to_thriller" onclick="rateBooksToGenre('thriller', '/user/rate_books?genre=thriller');
-                                            return false;">Thriller</a>
-                                </div>
-                                <div class="navRow">
-                                    <a href="/user/rate_books?genre=travel" class="actionLinkLite " id="nav_to_travel" onclick="rateBooksToGenre('travel', '/user/rate_books?genre=travel');
-                                            return false;">Travel</a>
-                                </div>
-                                <div class="navRow">
-                                    <a href="/user/rate_books?genre=young-adult" class="actionLinkLite " id="nav_to_young-adult" onclick="rateBooksToGenre('young-adult', '/user/rate_books?genre=young-adult');
-                                            return false;">Young Adult</a>
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
+                <?php $this->renderPartial("partial/bar_left", array('category_father' => $category_father, 'subject_type' => $subject_type)) ?>
 
                 <div class="three-fourths ">
-                    <h3 class="header-color" > Khoa học máy tính</h3>
+                    <h3> Khoa học máy tính</h3>
                     <div class="teacherList Side">
                         <div class="leftAlignedImage ">
                             <div class="coverWrapper">
