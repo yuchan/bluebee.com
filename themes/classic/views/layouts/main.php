189978d0766603e665f92086da11a4233b4aaf8f
Huy Nguyen <huynt57@gmail.com>
2014-08-01 21:46:15 +0700

    bug in main layout

--- themes/classic/views/layouts/main.php
+++ themes/classic/views/layouts/main.php
@@ -146,12 +146,14 @@
                     <body class="l-body home" style="background-color: white">
                         <div id="fb-root"></div>
                         <script>(function(d, s, id) {
-                          var js, fjs = d.getElementsByTagName(s)[0];
-                          if (d.getElementById(id)) return;
-                          js = d.createElement(s); js.id = id;
-                          js.src = "//connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v2.0";
-                          fjs.parentNode.insertBefore(js, fjs);
-                        }(document, 'script', 'facebook-jssdk'));</script>
+                                var js, fjs = d.getElementsByTagName(s)[0];
+                                if (d.getElementById(id))
+                                    return;
+                                js = d.createElement(s);
+                                js.id = id;
+                                js.src = "//connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v2.0";
+                                fjs.parentNode.insertBefore(js, fjs);
+                            }(document, 'script', 'facebook-jssdk'));</script>
                         <style>
                             #fan_page_fb {
                                 position: fixed;
@@ -283,8 +285,6 @@
                                                                         </a>
                                                                     </div>
                                                                 </div>
-
-
                                                                 <div class="w-nav-item level_1">
                                                                     <div class="w-nav-item-h">
                                                                         <a href="<?php echo Yii::app()->createUrl("share/teacherListPage") ?>" class="w-nav-anchor level_1">
@@ -295,48 +295,25 @@
                                                                     </div>
                                                                 </div>
 
-                                                                <div class="w-nav-item level_1">
-                                                                    <div class="w-nav-item-h">
-                                                                        <a id="login" href="<?php echo $this->createUrl('welcomePage/fb_login') ?>">Đăng nhập với facebook</a>
-                                                                    </div>
-                                                                </div>
-
-
-
-
+                                                                <?php
+                                                                if (Yii::app()->session["user_id"] == "")
+                                                                {
+                                                                    echo '
+                                                                      
 
                                                                 <div class="w-nav-item level_1">
                                                                     <div class="w-nav-item-h">
-                                                                        <a href="<?php echo Yii::app()->createUrl("user?token=" . Yii::app()->session['token']) ?>" class="w-nav-anchor level_ava">
-                                                                            <img style="border: 5px solid white;"class="ava" src="<?php
-                                                                            if (Yii::app()->session['user_avatar'] == "") {
-                                                                                echo Yii::app()->theme->baseUrl, "/assets/img/logo.jpg";
-                                                                            } else {
-
-                                                                                echo Yii::app()->session['user_avatar'];
-                                                                            }
-                                                                            ?>"/>
-                                                                        </a>
-
-
-                                                                        <div class="w-nav-list place_down show_onhover level_2">
-                                                                            <div class="w-nav-list-h">
-                                                                                <div class="w-nav-item level_2">
-                                                                                    <div class="w-nav-item-h">
-                                                                                        <a href="<?php echo Yii::app()->createUrl('welcomePage/logout') ?>" class="w-nav-anchor level_2">Đăng xuất</a>
-                                                                                    </div>
-                                                                                </div>
-                                                                                <!--                                                            <div class="w-nav-item level_2">
-                                                                                                                                                <div class="w-nav-item-h">
-                                                                                                                                                    <a href="home-parallax.html" class="w-nav-anchor level_2">Cập nhật thông tin</a>
-                                                                                                                                                </div>
-                                                                                                                                            </div>-->
-
-                                                                            </div>
-                                                                        </div>
-
+                                                                        <a id="login" href="'.<?php echo $this->createUrl('welcomePage/fb_login') ?>">.'Đăng nhập với facebook</a>
                                                                     </div>
-                                                                </div>
+                                                                </div>';
+                                                                }
+                                                                
+                                                                else
+                                                                {
+                                                                    
+                                                                }
+                                                                ?> 
+
 
 
                                                             </div>
@@ -384,7 +361,7 @@
                                   </div> -->
                                 <!-- MAIN -->
                                 <div style="padding-top: 126px">
-                                    <?php echo $content; ?>
+<?php echo $content; ?>
                                 </div>
                                 <!-- /MAIN -->
                             </div>
