44d2924b51458723e6ccef1e53596b059629ae67
Huy Nguyen <huynt57@gmail.com>
2014-08-03 16:05:13 +0700

    fix document controller

--- protected/controllers/DocumentController.php
+++ protected/controllers/DocumentController.php
@@ -116,11 +116,14 @@ class DocumentController extends BaseController {
     }
 
     public function saveDoc($doc_name, $doc_description, $doc_url, $doc_author, $subject_id, $doc_scribd_id, $doc_type, $doc_path, $doc_author_name) {
+        $doc_data = Subject::model()->findByAttributes(array('subject_id' => $subject_id));
         $doc_model = new Doc;
         $doc_model->doc_name = $doc_name;
         $doc_model->doc_description = $doc_description;
         $doc_model->doc_url = $doc_url;
         $doc_model->doc_path = $doc_path;
+        $doc_model->subject_faculty = $doc_data->subject_faculty;
+        $doc_model->subject_dept = $doc_data->subject_dept;
         $doc_model->doc_scribd_id = $doc_scribd_id;
         $doc_model->doc_type = $doc_type;
         $doc_model->doc_status = 1;
@@ -181,7 +184,7 @@ class DocumentController extends BaseController {
                         }
                         $tempFile = $_FILES['file']['tmp_name'];          //3
                         $targetPath = $storeFolder;  //4
-                        $targetFile = $targetPath .  $name ;  //5
+                        $targetFile = $targetPath . $name;  //5
                         $ext = pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION);
                         move_uploaded_file($tempFile, $targetFile); //6
                         $doc_path = Yii::app()->createAbsoluteUrl('uploads') . '/document/user_id_' . $doc_author . '/' . $name;
