551acc7a799ba361e30010f1c8a1fcc7b7b6c484
khoa le van <khoalevan1994@gmail.com>
2014-08-05 21:20:18 +0700

    save code

--- protected/models/Subject.php
+++ protected/models/Subject.php
@@ -59,7 +59,8 @@ class Subject extends CActiveRecord
 	{
 		// NOTE: you may need to adjust the relation name and the related
 		// class name for the relations automatically generated below.
-		return array(
+		return array( 'subject_doc' => array (self::BELONGS_TO, 'SubjectDoc', array('subject_id'=>'subject_id'))
+                    //'docs' => array(self::BELONGS_TO, 'SubjectDoc', 'doc_id')
 		);
 	}
 
